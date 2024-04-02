9be618c75784942f0c3737e59312fae33ae2cbbb
Tarkio <wowsendarpm@gmail.com>
2024-04-02 23:45:38 +0300

    reformat code

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,6 +17,7 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
     @Test
     void findSecondMax() {
         StatisticsService service = new StatisticsService();
